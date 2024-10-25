1c5023a37ef9d2871fc57d76681d1e23e98b985b
tunglt-bayern <tunglt.bayern@gmail.com>
2024-10-26 05:10:53 +0700

    module 2.2 SectionList fix #4

--- app/MainMenu.js
+++ app/MainMenu.js
@@ -1,24 +1,34 @@
 import * as React from 'react';
-import { ScrollView, Text, View, StyleSheet, FlatList } from 'react-native';
+import { Text, View, StyleSheet, SectionList } from 'react-native';
 
 const menuItemsToDisplay = [
-  { name: 'Hanoi - Saigon', price: '$5.00', id: '1A' },
-  { name: 'Hanoi - Da Nang', price: '$5.00', id: '2B' },
-  { name: 'Hanoi - Phu Quoc', price: '$7.50', id: '3C' },
-  { name: 'Saigon - Hanoi', price: '$5.00', id: '4D' },
-  { name: 'Saigon - Da Nang', price: '$5.00', id: '5E' },
-  { name: 'Saigon - Phu Quoc', price: '$8.50', id: '6F' },
-  { name: 'Ha noi - Da Nang - Saigon', price: '$10.00', id: '7G' },
-  { name: 'Ha Noi - Saigon - Phu Quoc', price: '$14.00', id: '8H' },
-  { name: 'Sai gon - Da Nang - Hanoi', price: '$11.00', id: '9I' },
-  { name: 'Sai gon - Phu Quoc - Da Nang - Hanoi', price: '$15.50', id: '10J' },
-  { name: 'Hanoi - Ha Giang', price: '$3.00', id: '11K' },
-  { name: 'Hanoi - Ha Long', price: '$3.00', id: '12L' },
-  { name: 'Hanoi - Ha Long - Ha Giang', price: '$3.00', id: '13M' },
-  { name: 'Saigon - Vung Tau - Phu Quoc', price: '$3.00', id: '14N' },
-  { name: 'Saigon - Nha Trang - Phu Quoc', price: '$3.75', id: '15O' },
-  { name: 'Saigon - Vung Tau - Da Lat', price: '$6.00', id: '16Q' },
-  { name: 'Saigon - Nha Trang - Da Lat', price: '$4.00', id: '17R' },
+  {
+  title: 'From Hanoi',
+  data: [
+    { name: 'Hanoi - Saigon', price: '$5.00' },
+    { name: 'Hanoi - Da Nang', price: '$5.00' },
+    { name: 'Hanoi - Phu Quoc', price: '$7.50' },
+    { name: 'Hanoi - Da Nang - Saigon', price: '$10.00' },
+    { name: 'Hanoi - Saigon - Phu Quoc', price: '$14.00' },
+    { name: 'Hanoi - Ha Giang', price: '$3.00' },
+    { name: 'Hanoi - Ha Long', price: '$3.00' },
+    { name: 'Hanoi - Ha Long - Ha Giang', price: '$3.00' },
+  ],
+  },
+  {
+  title: 'From Saigon',
+  data: [
+    { name: 'Saigon - Hanoi', price: '$5.00' },
+    { name: 'Saigon - Da Nang', price: '$5.00' },
+    { name: 'Saigon - Phu Quoc', price: '$8.50' },
+    { name: 'Sai gon - Da Nang - Hanoi', price: '$11.00' },
+    { name: 'Sai gon - Phu Quoc - Da Nang - Hanoi', price: '$15.50' },
+    { name: 'Saigon - Vung Tau - Phu Quoc', price: '$3.00' },
+    { name: 'Saigon - Nha Trang - Phu Quoc', price: '$3.75' },
+    { name: 'Saigon - Vung Tau - Da Lat', price: '$6.00' },
+    { name: 'Saigon - Nha Trang - Da Lat', price: '$4.00' },
+  ], 
+  },
 ];
 
 const Item = ({ name, price }) => (
@@ -31,17 +41,23 @@ const Item = ({ name, price }) => (
 const MenuItems = () => {
   const renderItem = ({ item }) => <Item name={item.name} price={item.price} />;
 
+  const renderSectionHeader = ({ section: { title } }) => (
+    <View style={menuStyles.headerStyle}>
+      <Text style={menuStyles.sectionHeader}>{title}</Text>
+    </View>
+  );
+
   return (
     <View style={menuStyles.container}>
-      <FlatList
-        data={menuItemsToDisplay}
-        keyExtractor={(item) => item.id}
-        renderItem={renderItem}></FlatList>
+      <SectionList
+        sections={menuItemsToDisplay}
+        keyExtractor={(item, index) => item.name + index}
+        renderItem={renderItem}
+        renderSectionHeader={renderSectionHeader}></SectionList>
     </View>
   );
 };
 
-// Add styles to the component
 const menuStyles = StyleSheet.create({
   container: {
     flex: 1,
@@ -55,6 +71,15 @@ const menuStyles = StyleSheet.create({
   itemText: {
     color: '#F4CE14',
     fontSize: 20,
+  },
+  headerStyle: {
+    backgroundColor: '#F4CE14',
+  },
+  sectionHeader: {
+    color: 'black',
+    fontSize: 26,
+    flexWrap: 'wrap',
+    textAlign: 'center',
   },
 });
 
