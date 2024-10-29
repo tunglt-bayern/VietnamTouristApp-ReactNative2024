b237cfe723dfbb9f1cd9079cbee309b0205d816d
tunglt-bayern <tunglt.bayern@gmail.com>
2024-10-30 06:58:04 +0700

    Module 3. Image & Hook / 3.1 Image fix #8

--- app/WelcomeScreen.js
+++ app/WelcomeScreen.js
@@ -1,89 +1,54 @@
-import React, { useState } from 'react';
-import {
-  ScrollView,
-  Text,
-  StyleSheet,
-  TextInput,
-  Pressable,
-} from 'react-native';
+  import { ScrollView, View, Text, StyleSheet, Image } from 'react-native';
 
-export default function LoginScreen() {
-  const [email, onChangeEmail] = useState('');
-  const [password, onChangePassword] = useState('');
-  const [loggedIn, onLogin] = useState(false);
-
-  return (
-    <ScrollView style={styles.container}>
-      <Text style={styles.headerText}>Welcome to Vietnam Local</Text>
-      {loggedIn && <Text style={styles.headerText}>You are logged in!</Text>}
-
-      {!loggedIn && (
-        <>
-          <Text style={styles.regularText}>Login to continue </Text>
-          <TextInput
-            style={styles.inputBox}
-            value={email}
-            onChangeText={onChangeEmail}
-            placeholder={'email'}
-            keyboardType={'email-address'}
+  export default function WelcomeScreen() {
+    return (
+      <ScrollView style={styles.container}>
+        <View style={styles.headerWrapper}>
+          <Image
+            style={styles.image}
+            source={require('../img/logo.png')}
+            resizeMode="cover"
+            accessible={true}
+            accessibilityLabel={'Vietnam Local'}
           />
-          <TextInput
-            style={styles.inputBox}
-            value={password}
-            onChangeText={onChangePassword}
-            placeholder={'password'}
-            keyboardType={'default'}
-            secureTextEntry={true}
-          />
-          <Pressable onPress={() => onLogin(!loggedIn)} style={styles.button}>
-            <Text style={styles.buttonText}>Log in</Text>
-          </Pressable>
-        </>
-      )}
-    </ScrollView>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  headerText: {
-    padding: 40,
-    fontSize: 30,
-    color: '#EDEFEE',
-    textAlign: 'center',
-  },
-  regularText: {
-    fontSize: 24,
-    padding: 20,
-    marginVertical: 8,
-    color: '#EDEFEE',
-    textAlign: 'center',
-  },
-  inputBox: {
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
-    fontSize: 16,
-    borderColor: 'EDEFEE',
-    backgroundColor: '#EDEFEE',
-  },
-  button: {
-    fontSize: 22,
-    padding: 10,
-    marginVertical: 8,
-    margin: 100,
-    backgroundColor: '#EE9972',
-    borderColor: '#EE9972',
-    borderWidth: 2,
-    borderRadius: 50,
-  },
-  buttonText: {
-    color: 'black',
-    textAlign: 'center',
-    fontSize: 25,
-  },
-});
+          <Text style={styles.headerText}>Vietnam Local</Text>
+        </View>
+        <Text style={styles.regularText}>
+        Vietnam Local was born with the mission to help international friends travel more easily and enjoy more fun while visiting Vietnam!
+        </Text>
+      </ScrollView>
+    );
+  }
 
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+    headerWrapper: {
+      flexDirection: 'row',
+      justifyContent: 'center',
+      margin: 10,
+    },
+    headerText: {
+      paddingRight: 10,
+      paddingLeft: 20,
+      paddingTop: 30,
+      paddingBottom: 10,
+      fontSize: 30,
+      color: '#EDEFEE',
+      textAlign: 'center',
+    },
+    regularText: {
+      fontSize: 24,
+      padding: 20,
+      marginVertical: 8,
+      color: '#EDEFEE',
+      textAlign: 'center',
+    },
+    image: {
+      width: 100,
+      height: 100,
+      borderRadius: 20,
+    },
+  });
--- app/index.tsx
+++ app/index.tsx
@@ -17,7 +17,7 @@ export default function App() {
       </View>
     </>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
