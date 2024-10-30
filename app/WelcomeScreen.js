71e45ae54c2303e5e89be50e42bb391ccd47a9cd
tunglt-bayern <tunglt.bayern@gmail.com>
2024-10-30 08:05:28 +0700

    Module 3. Image & Hook / 3.2 Hooks fix #9

--- app/WelcomeScreen.js
+++ app/WelcomeScreen.js
@@ -1,54 +1,81 @@
-  import { ScrollView, View, Text, StyleSheet, Image } from 'react-native';
+import {
+  ScrollView,
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  useColorScheme,
+} from 'react-native';
 
-  export default function WelcomeScreen() {
-    return (
-      <ScrollView style={styles.container}>
-        <View style={styles.headerWrapper}>
-          <Image
-            style={styles.image}
-            source={require('../img/logo.png')}
-            resizeMode="cover"
-            accessible={true}
-            accessibilityLabel={'Vietnam Local'}
-          />
+export default function WelcomeScreen() {
+  const colorScheme = useColorScheme();
 
-          <Text style={styles.headerText}>Vietnam Local</Text>
-        </View>
-        <Text style={styles.regularText}>
-        Vietnam Local was born with the mission to help international friends travel more easily and enjoy more fun while visiting Vietnam!
+  return (
+    <ScrollView
+      style={[
+        styles.container,
+        colorScheme === 'light'
+          ? { backgroundColor: '#fff' }
+          : { backgroundColor: '#333333' },
+      ]}>
+      <View style={styles.headerWrapper}>
+        <Image
+          style={styles.image}
+          source={require('../img/logo.png')}
+          resizeMode="cover"
+          accessible={true}
+          accessibilityLabel={'Vietnam Local Logo'}
+        />
+
+        <Text
+          style={[
+            styles.headerText,
+            colorScheme === 'light'
+              ? { color: '#333333' }
+              : { color: '#EDEFEE' },
+          ]}>
+          Vietnam Local
         </Text>
-      </ScrollView>
-    );
-  }
+      </View>
+      <Text
+        style={[
+          styles.regularText,
+          colorScheme === 'light' ? { color: '#333333' } : { color: '#EDEFEE' },
+        ]}>
+        Vietnam Local was born with the mission to help international friends travel more easily and enjoy more fun while visiting Vietnam!
+      </Text>
+    </ScrollView>
+  );
+}
 
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-    },
-    headerWrapper: {
-      flexDirection: 'row',
-      justifyContent: 'center',
-      margin: 10,
-    },
-    headerText: {
-      paddingRight: 10,
-      paddingLeft: 20,
-      paddingTop: 30,
-      paddingBottom: 10,
-      fontSize: 30,
-      color: '#EDEFEE',
-      textAlign: 'center',
-    },
-    regularText: {
-      fontSize: 24,
-      padding: 20,
-      marginVertical: 8,
-      color: '#EDEFEE',
-      textAlign: 'center',
-    },
-    image: {
-      width: 100,
-      height: 100,
-      borderRadius: 20,
-    },
-  });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  headerWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    margin: 10,
+  },
+  headerText: {
+    paddingRight: 10,
+    paddingLeft: 20,
+    paddingTop: 30,
+    paddingBottom: 10,
+    fontSize: 30,
+    color: '#EDEFEE',
+    textAlign: 'center',
+  },
+  regularText: {
+    fontSize: 24,
+    padding: 20,
+    marginVertical: 8,
+    color: '#EDEFEE',
+    textAlign: 'center',
+  },
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius: 20,
+  },
+});
