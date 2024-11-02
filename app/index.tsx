d03c9ec320a557ef1871ad7bed3a5a31723d4dea
tunglt-bayern <tunglt.bayern@gmail.com>
2024-11-02 09:09:14 +0700

    Module 4.1 fix #10

--- app/index.tsx
+++ app/index.tsx
@@ -4,25 +4,39 @@ import { View, StyleSheet } from 'react-native';
 import ZodiacHeader from './ZodiacHeader';
 import ZodiacFooter from './ZodiacFooter';
 import WelcomeScreen from './WelcomeScreen';
+import MainMenu from './MainMenu';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <>
+    <NavigationContainer independent={true}>
       <View style={styles.container}>
         <ZodiacHeader />
-        <WelcomeScreen />
-      </View>
-      <View style={styles.footerContainer}>
+        <View style={styles.content}>
+          <Stack.Navigator initialRouteName="Menu">
+            <Stack.Screen name="Welcome" component={WelcomeScreen} />
+            <Stack.Screen name="Menu" component={MainMenu} />
+          </Stack.Navigator>
+        </View>
         <ZodiacFooter />
       </View>
-    </>
+    </NavigationContainer>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#333333',
   },
-  footerContainer: { backgroundColor: '#333333' },
+  content: {
+    flex: 1,
+  },
+  footerContainer: {
+    backgroundColor: '#333333',
+    paddingVertical: 10,
+  },
 });
