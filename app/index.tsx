578972abac30b4e708f29c9066fb3dafc95628ee
tunglt-bayern <tunglt.bayern@gmail.com>
2024-11-10 00:27:04 +0700

    Module 4. Navigation / 4.4 Drawer Navigation fix 13

--- app/index.tsx
+++ app/index.tsx
@@ -1,53 +1,38 @@
 
-  import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 
-  import ZodiacHeader from './ZodiacHeader';
-  import ZodiacFooter from './ZodiacFooter';
-  import WelcomeScreen from './WelcomeScreen';
-  import MainMenu from './MainMenu';
-  import { NavigationContainer } from '@react-navigation/native';
-  import { createNativeStackNavigator } from '@react-navigation/native-stack';
-  import React, { useState } from 'react';
-  import Ionicons from '@expo/vector-icons/Ionicons';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import ZodiacHeader from './ZodiacHeader';
+import ZodiacFooter from './ZodiacFooter';
+import WelcomeScreen from './WelcomeScreen';
+import MainMenu from './MainMenu';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
-  const Tab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
 
-  export default function App() {
-    return (
-      <>
-        <NavigationContainer independent={true}>
-          <View style={styles.container}>
-            <ZodiacHeader />
-            <Tab.Navigator
-              screenOptions={({ route }) => ({
-                tabBarIcon: ({size}) => {
-                  let iconName;
-                  if (route.name === 'Welcome') {
-                    iconName = 'md-home';
-                  } else if (route.name === 'Menu') {
-                    iconName = 'md-menu';
-                  }
-                  return <Ionicons name={iconName} size={size} />;
-                },
-              })}
-              initialRouteName="Menu">
-              <Tab.Screen name="Welcome" component={WelcomeScreen} />
-              <Tab.Screen name="Menu" component={MainMenu} />
-            </Tab.Navigator>
-          </View>
-          <View style={styles.footerContainer}>
-            <ZodiacFooter />
-          </View>
-        </NavigationContainer>
-      </>
-    );
-  }
+export default function App() {
+  return (
+    <>
+      <NavigationContainer independent={true}>
+        <View style={styles.container}>
+          <ZodiacHeader />
+          <Drawer.Navigator useLegacyImplementation initialRouteName="Menu">
+            <Drawer.Screen name="Welcome" component={WelcomeScreen} />
+            <Drawer.Screen name="Menu" component={MainMenu} />
+          </Drawer.Navigator>
+        </View>
+        <View style={styles.footerContainer}>
+          <ZodiacFooter />
+        </View>
+      </NavigationContainer>
+    </>
+  );
+}
 
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#333333',
-    },
-    footerContainer: { backgroundColor: '#333333' },
-  })
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#333333',
+  },
+  footerContainer: { backgroundColor: '#333333' },
+});
